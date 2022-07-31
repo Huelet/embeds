cf3ae452856f1e3bb5127e6bcf4210ac8df4100f
Levi R <levir620@skiff.com>
2022-07-31 15:44:46 -0700

    🔧 update gatsby config

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -3,11 +3,8 @@ import type { GatsbyConfig } from "gatsby";
 const config: GatsbyConfig = {
   siteMetadata: {
     title: `Huelet Embeds`,
-    siteUrl: `https://www.yourdomain.tld`
+    siteUrl: `https://publish.huelet.net`
   },
-  // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
-  // If you use VSCode you can also use the GraphQL plugin
-  // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
   plugins: ["gatsby-plugin-emotion", "gatsby-plugin-image", "gatsby-plugin-react-helmet", "gatsby-plugin-sitemap", {
     resolve: 'gatsby-plugin-manifest',
