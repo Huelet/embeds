77c4d87db58f67aa2aebc656484d779755b5e5b0
Levi R <levir620@skiff.com>
2022-08-01 15:56:29 -0700

    ✨ add all the features

--- src/pages/Player.tsx
+++ src/pages/Player.tsx
@@ -1,5 +1,44 @@
 import * as React from "react";
+import VideoPlayer from "griffith";
 
-export const Player = () => {
-  return <div>Player</div>;
+const Player = () => {
+  const [vuid, setVuid] = React.useState("");
+  const [videoUrl, setVideoUrl] = React.useState({});
+  React.useEffect(() => {
+    const url = new URL(
+      typeof window === "undefined"
+        ? "https://example.com/etc/etc"
+        : location.href
+    );
+    setVuid(url.searchParams.get("vuid") as string);
+  });
+
+  React.useEffect(() => {
+    const getVideoData = async () => {
+      console.log(`https://api.huelet.net/videos/lookup/${vuid}`);
+
+      const resp = await fetch(`https://api.huelet.net/videos/lookup/${vuid}`);
+      const data = await resp.json();
+      data.data.url_webm
+        ? setVideoUrl(data.data.url_webm)
+        : setVideoUrl(data.data.url);
+    };
+    getVideoData();
+  }, [vuid]);
+  return (
+    <div>
+      <VideoPlayer
+        id={`player-${Math.round(
+          Date.now() + Math.random() * Date.now()
+        ).toString(36)}`}
+        sources={
+          videoUrl
+            ? { hd: { play_url: videoUrl } }
+            : { hd: { play_url: videoUrl } }
+        }
+      />
+    </div>
+  );
 };
+
+export default Player;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,26 +1,127 @@
 /** @jsx jsx */
 import * as React from "react";
 import { css, jsx } from "@emotion/react";
-import { Card } from "@huelet/foundation-ui";
-import { Player } from "./Player";
+import { Button, Card } from "@huelet/foundation-ui";
+import Player from "./Player";
+import { Copy } from "@fdn-ui/icons-react";
 
 const IndexPage = () => {
-  const url = new URL(location.href);
-  const embed = Boolean(url.searchParams.get("embed"));
-  const vuid = url.searchParams.get("vuid");
-
+  const [embed, setEmbed] = React.useState(false);
+  const [videoUrl, setVideoUrl] = React.useState("");
+  const [embedResp, setEmbedResponse] = React.useState("");
+  React.useEffect(() => {
+    const url = new URL(
+      typeof window === "undefined"
+        ? "https://example.com/etc/etc"
+        : location.href
+    );
+    setEmbed(Boolean(url.searchParams.get("embed")));
+  }, [videoUrl]);
   React.useEffect(() => {
     if (embed) {
       document.body.classList.add("embed");
     }
   }, [embed]);
+  const fetchCode = async () => {
+    const code = `
+      <iframe
+        src="https://publish.huelet.net/?embed=true&vuid=${
+          new URL("https://huelet.net/w/15xqiownj1672fjysg/").pathname.split(
+            "/"
+          )[2]
+        }"
+        width="100%"
+        height="100%"
+        frameborder="0"
+        allow="accelerometer; autoplay; encrypted-media; picture-in-picture"
+        allowfullscreen
+      ></iframe>
+    `;
+    setEmbedResponse(code);
+  };
   return (
     <main>
       {embed ? (
         <Player />
       ) : (
-        <Card full={true}>
-          <div>hi</div>
+        <Card full={true} title="Embed a video">
+          <div
+            css={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              justifyContent: "center",
+              height: "100%",
+              padding: "1rem",
+            }}
+          >
+            <form>
+              <input
+                type="text"
+                placeholder="Paste a video URL"
+                name="vuid"
+                value={videoUrl}
+                onChange={(e) => {
+                  e.preventDefault();
+                  setVideoUrl(e.target.value);
+                }}
+                css={{
+                  width: "100%",
+                  padding: "0.5rem",
+                  background: "transparent",
+                  border: "2px solid #7600ff",
+                  color: "#ffffff",
+
+                  "&::-webkit-input-placeholder": {
+                    color: "#ffffff",
+                  },
+                }}
+              />
+
+              <Button>
+                <span
+                  onClick={() => fetchCode()}
+                  css={{
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "center",
+                    width: "100%",
+                    height: "100%",
+                  }}
+                >
+                  Embed
+                </span>
+              </Button>
+            </form>
+
+            <div>
+              {embedResp ? (
+                <div>
+                  <Button>
+                    <span
+                      onClick={() => {
+                        navigator.clipboard.writeText(embedResp);
+                        setEmbedResponse("Copied!");
+                      }}
+                      css={{
+                        display: "flex",
+                        alignItems: "center",
+                        justifyContent: "center",
+                        width: "100%",
+                        height: "100%",
+                      }}
+                    >
+                      <Copy fill="white" />
+                      Copy to Clipboard
+                    </span>
+                  </Button>
+                  <pre>
+                    <code>{embedResp}</code>
+                  </pre>
+                </div>
+              ) : null}
+            </div>
+          </div>
         </Card>
       )}
     </main>
